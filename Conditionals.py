7d35eebba51f73f54c9727c42938312ffe23b39c
Bow14 <54337048+Bow14@users.noreply.github.com>
2021-09-16 21:33:32 -0600

    Add files via upload

---
+++ Conditionals.py
@@ -0,0 +1,13 @@
+import sys
+x = int(100)
+
+
+
+#if x <= 0 or 25:
+    #print(x,"is between 0 and 25")
+#elif x <= 75 or 100:
+    #print("or 75 and 100")
+#else:
+    #print("You are wrong")
+if x <= 25 or (x >= 75 and x <= 100):
+    print(x,"is between 0 and 25 or 75 and 100")
