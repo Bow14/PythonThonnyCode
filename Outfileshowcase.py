33db7d0b540ef3fef80e3ea6d0088b43c5879bbf
Bow14 <54337048+Bow14@users.noreply.github.com>
2021-10-05 15:58:37 -0600

    Add files via upload

---
+++ Outfileshowcase.py
@@ -0,0 +1,6 @@
+out_file = open('greetings',"w") #w is  the writer mode, r is reading mode 
+out_file.write("Hello")
+out_file.close()
+
+# or you could do in_file type stuff
+#print(infile.read()) reads the line or  allows you to read the line
