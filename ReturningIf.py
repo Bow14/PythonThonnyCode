fa786f2fcaf64acda75513109068024877d29f32
Bow14 <54337048+Bow14@users.noreply.github.com>
2021-10-19 23:57:48 -0600

    Add files via upload

---
+++ ReturningIf.py
@@ -0,0 +1,14 @@
+def main():
+    pep = 4
+    
+    ap = 9
+    
+    if pep <= 4:
+        print("Go ahead")
+    
+    else:
+        print("Bet not today")
+    
+    
+if __name__ == '__main__':
+    main()
