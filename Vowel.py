5be527a7e1a220966089b53994e5d951de0ac111
Bow14 <54337048+Bow14@users.noreply.github.com>
2021-11-16 22:48:27 -0700

    Add files via upload

---
+++ Vowel.py
@@ -0,0 +1,17 @@
+##########################################################
+# Erase the variable my_string before submitting your work
+##########################################################
+
+my_string = ""
+
+
+##########################################################
+# Enter your code below
+##########################################################
+
+vowel = 'aeiou'
+
+for vowel in vowel:
+    my_string = my_string.replace(vowel, "*")
+
+print(my_string)
