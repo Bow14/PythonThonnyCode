c991c0b32dfff22c65dbc91af2fe97dd08a5fd73
Bow14 <54337048+Bow14@users.noreply.github.com>
2021-11-18 23:44:31 -0700

    Add files via upload

---
+++ Randstamp.py
@@ -0,0 +1,26 @@
+from random import choice
+import turtle
+import math
+from statistics import mean
+
+def main():
+    length, walks = 1000, 50
+    steps = [(0,1),(1,0),(0,-1),(-1,0)]
+    bo = turtle.Turtle()
+    end_points = []
+    
+    #for each walk of 1000 steps, find the endpoint, add to list
+    for i in range(walks):
+        point = [0,0]
+        for j in range(length):
+            step = choice(steps)
+            point[0] += step[0]
+            point[1] += step[1]
+        end_points.append(point)
+    print(end_points)
+            
+    
+    
+    
+if __name__ == '__main__':
+    main()
