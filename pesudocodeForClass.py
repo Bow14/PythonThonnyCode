23c50b795fa654fc79f4bab897b695ca3ac0cefe
Bow14 <54337048+Bow14@users.noreply.github.com>
2021-12-10 15:09:35 -0700

    Add files via upload

---
+++ pesudocodeForClass.py
@@ -0,0 +1,25 @@
+'''
+Starting with opening BLS_private.csv as a read file.
+Make an if statement for years below and start sorting democrats years from republicans years
+1: Open “BLS_private.csv” as a ‘w’ file with keeping it a .csv file with the following delimiter of a ,
+1.5 make an empty dict {} for step 2 ex… Republican_Job_Count[] and  Democrat _Job_Count[]
+ 2: An if statement taking the democratic years and sorting them into their own dictionary (if we can shorten it by doing through years)
+2.5 for every /n sum up the jobs of that year for democrats
+3: An if statement taking the republican years and sorting them into their own dictionary (if we can shorten it by doing through years)
+3.5: for every /n sum up the jobs of that year for democrats 
+3.8 print out the values for the code above for testing purposes 
+4: Make a Def function called compare (Clinitons assertion) with our own printed results 
+4.5 Write results to a file named "conclusions.md".
+5: We write in the conclusion.md of our thoughts about the political statement at hand
+6; Put pseudocode in presidents.txt
+
+
+List of presidents by year:
+1961 -1968 Dem
+1969-1976 Rep
+1977-1980 Dem
+1981 – 1992 Rep
+1993 – 2000 Dem
+2001 – 2008 Rep
+2009 – 2012 Dem
+'''
