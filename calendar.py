0cff509559cb06d3dc4dab6e9dce8aedbd499d83
Bow14 <54337048+Bow14@users.noreply.github.com>
2021-12-23 22:52:52 -0700

    Testing the calendar import, it doesnt really work

---
+++ calendar.py
@@ -0,0 +1,9 @@
+
+import calendar
+
+y = int(input("Please enter year : " ))
+m = int(input("Please enter month : " ))
+d = int(input("Please enter day : " ))
+
+print("The date of this time line is : ")
+print((y, m, d))
