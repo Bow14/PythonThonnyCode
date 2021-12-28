2f90f99106d162c0b5c51d9805eab0bd10c04e51
Bow14 <54337048+Bow14@users.noreply.github.com>
2021-12-27 21:07:30 -0700

    Add files via upload

---
+++ MyClintonAnalist.py
@@ -0,0 +1,17 @@
+'''
+The Start of Bill clintions answer analisist
+'''
+democrat_jobs = 0
+republican_jobs = 0
+parameters = []
+list_of_presidents = []
+
+def recieve_presidents_info(contents_presidents)
+    for line in contents_presidents
+        list_of_presidents.append(line.split().strip(','))
+    
+def gather_data(the_contents)
+    for line in the_contents
+        parameters.append(line.split().strip(','))
+        
+    
