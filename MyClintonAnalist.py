ab38d0b173d4ec820c086ea174642dd881c04b85
Bow14 <54337048+Bow14@users.noreply.github.com>
2021-12-30 19:20:39 -0700

    Added the final conclusion paragraph
    About to start my data sorting to calculate the math involved.

--- MyClintonAnalist.py
+++ MyClintonAnalist.py
@@ -1,10 +1,27 @@
 '''
 The Start of Bill clintions answer analisist
 '''
+import csv
+import sys
+
 democrat_jobs = 0
 republican_jobs = 0
 parameters = []
 list_of_presidents = []
+
+def conclusion_formating(conclusions)
+    clintons_statement = 42000000
+    
+    conclusions.write('Republican Job count **{}**'.format(republican_jobs))
+    conclusions.write('Democrat Job count **{}**'.format(democrat_jobs))
+    
+    if democrat_conclusion >= clinton_assertion:
+        conclusion.write('Clintons assertion was right into claiming on how many jobs were made or were different when the president was republican and or democratic')
+        conclusion.write("Clintons numbers were right with the statement that follows: 'Republicans 24 million, Democrats 42 million'")
+    else:
+        conclusion.write('Clinton was wrong in his job count and that is unfortuante')
+        
+def data_sort():
 
 def recieve_presidents_info(contents_presidents)
     for line in contents_presidents
@@ -13,5 +30,7 @@ def recieve_presidents_info(contents_presidents)
 def gather_data(the_contents)
     for line in the_contents
         parameters.append(line.split().strip(','))
+
+def main()
         
     
